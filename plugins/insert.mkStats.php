508fa8731e380dd68c64ffeb5b9b88f3f416e02a
x2000habouzit <x2000habouzit>
2004-01-27 09:23:48 +0000

    soucis avec les url relatives

--- plugins/insert.mkStats.php
+++ plugins/insert.mkStats.php
@@ -7,6 +7,15 @@ function nb_trk($pri,$tr) {
     return ($res?$res:"-");
 }
 
+function url()
+{
+  $chemins = Array('.', '..', '../..');
+  foreach ($chemins as $ch) {
+    if (file_exists("$ch/login.php"))
+      return "$ch";
+  }
+  return "";
+}
 /*
  * Smarty plugin
  * ------------------------------------------------------------- 
@@ -24,6 +33,8 @@ function smarty_insert_mkStats($params, &$smarty)
     $stats_req = ($stats_req ? $stats_req : "-");
 
     $nbtrk = array(nb_trk(5,2), nb_trk(4,2), nb_trk(5,1), nb_trk(4,1));
+
+    $rel = url();
     return <<<EOF
         <table class="bicol"
           style="font-weight:normal;text-align:center; border-left:0px; border-right:0px; margin-top:0.5em; width:100%; margin-left: 0;">
@@ -33,14 +44,14 @@ function smarty_insert_mkStats($params, &$smarty)
           <th width="33%">Todo</th>
         </tr>
         <tr class="impair">
-          <td><a href="admin/valider.php">$stats_req</a></td>
+          <td><a href="$rel/admin/valider.php">$stats_req</a></td>
           <td>
-            <a href="trackers/tracker_show.php?tr_id=2">
+            <a href="$rel/trackers/tracker_show.php?tr_id=2">
             <strong>{$nbtrk[0]}</strong> / {$nbtrk[1]}
             </a>
           </td>
           <td>
-            <a href="trackers/tracker_show.php?tr_id=1">
+            <a href="$rel/trackers/tracker_show.php?tr_id=1">
             <strong>{$nbtrk[2]}</strong> / {$nbtrk[3]}
             </a>
           </td>
