42cf65214b8dc93a83d6d7bee0feb2a5a9ac2b7e
x2000habouzit <x2000habouzit>
2004-07-19 13:43:27 +0000

    one easy one more

---
+++ htdocs/index.coupure.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML PUBLIC "-//W4C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <title>Anciens eleves Polytechnique, annuaire email, etc</title>
+    <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">
+    <link href="/css/default.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <table align="center" border=0 width=600>
+      <tr>
+        <td>
+          <img src="Sk/Base/logo.jpg">
+        </td>
+        <td>
+          <h1>Polytechnique.org</h1>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="2">
+          <h2>Service Web temporairement interrompu</h2>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="2">
+          <p>En pr&eacute;vision d'une coupure de courant ind&eacute;pendante de
+          notre volont&eacute;, le service web est interrompu.  Ceci nous permet
+          de garantir l'int&eacute;grit&eacute; des donn&eacute;es
+          stock&eacute;es sur nos serveurs. La continuit&eacute; du service mail
+          est assur&eacute; pendant la dur&eacute;e de la coupure.
+          </p>
+          <p>Nos excuses pour le d&eacute;rangement occasionn&eacute; et
+          &agrave; bient&ocirc;t!</p>
+          <p align="right"><b>L'&eacute;quipe de Polytechnique.org</b></p>
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
