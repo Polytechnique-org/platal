0d2a7d4471dc7ab992fd2dbda2de04d1d9fc41ac
x2000habouzit <x2000habouzit>
2004-08-24 19:34:21 +0000

    better url plugin

--- plugins/modifier.url.php
+++ plugins/modifier.url.php
@@ -3,11 +3,11 @@ function smarty_modifier_url($string)
 {
     if(strpos($string, "http://")!==false)
 	return $string;
-    $chemins = Array('.', '..', '/');
+    $chemins = Array('', '../', '../../');
     foreach ($chemins as $ch) {
-	if (file_exists("$ch/login.php") || file_exists("$ch/public/login.php"))
-	    return "$ch/$string";
+	if (file_exists($ch.'../htdocs/'))
+	    return $ch.$string;
     }
-    return "";
+    return '';
 }
 ?>
