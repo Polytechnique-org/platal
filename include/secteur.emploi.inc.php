a272f4d4915a229e7f73d29d3e8c22fd1cfe5f6a
x2000habouzit <x2000habouzit>
2004-08-31 20:27:59 +0000

    no more echos

--- include/secteur.emploi.inc.php
+++ include/secteur.emploi.inc.php
@@ -18,43 +18,45 @@
  *  Foundation, Inc.,                                                      *
  *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
  ***************************************************************************
-        $Id: secteur.emploi.inc.php,v 1.5 2004-08-31 16:04:11 x2000habouzit Exp $
+        $Id: secteur.emploi.inc.php,v 1.6 2004-08-31 20:27:59 x2000habouzit Exp $
  ***************************************************************************/
 
 
 function select_secteur($secteur){
     global $globals;
     if($secteur == '') $secteur = -1;
-    echo "<option value=\"\" ". (($secteur == '')?"selected='selected'":"") .">&nbsp;</option>\n";
+    $html = "<option value=\"\" ". (($secteur == '')?"selected='selected'":"") .">&nbsp;</option>\n";
     $res = $globals->db->query("SELECT id, label FROM emploi_secteur");
     while(list($tmp_id, $tmp_label) = mysql_fetch_row($res)){
-	echo "<option value=\"$tmp_id\" " . (($secteur == $tmp_id)?"selected='selected'":"") . ">$tmp_label</option>\n";
+	$html .= "<option value=\"$tmp_id\" " . (($secteur == $tmp_id)?"selected='selected'":"") . ">$tmp_label</option>\n";
     }
     mysql_free_result($res);
+    return $html;
 }
 
 function select_ss_secteur($secteur,$ss_secteur){
     global $globals;
     if($secteur != ''){
-	echo "<option value=\"\">&nbsp;</option>\n";
+	$html = "<option value=\"\">&nbsp;</option>\n";
 	$res = $globals->db->query("SELECT id, label FROM emploi_ss_secteur WHERE secteur = '$secteur'");
 	while(list($tmp_id, $tmp_label) = mysql_fetch_row($res)){
-	    echo "<option value=\"$tmp_id\" ". (($ss_secteur == $tmp_id)?"selected='selected'":"") .">$tmp_label</option>\n";
+	    $html .= "<option value=\"$tmp_id\" ". (($ss_secteur == $tmp_id)?"selected='selected'":"") .">$tmp_label</option>\n";
 	}
 	mysql_free_result($res);
+	return $html;
     }
     else{
-	echo "<option value=\"\" selected='selected'>&nbsp;</option>\n";
+	return "<option value=\"\" selected='selected'>&nbsp;</option>\n";
     }
 }
 
 //fonctions pour smarty
 function _select_secteur_smarty($params){
-  select_secteur($params['secteur']);
+    return select_secteur($params['secteur']);
 }
+
 function _select_ss_secteur_smarty($params){
-  if(!isset($params['secteur'])) return;
-  select_ss_secteur($params['secteur'], $params['ss_secteur']);
+    return select_ss_secteur($params['secteur'], $params['ss_secteur']);
 }
 $page->register_function('select_secteur', '_select_secteur_smarty');
 $page->register_function('select_ss_secteur', '_select_ss_secteur_smarty');
