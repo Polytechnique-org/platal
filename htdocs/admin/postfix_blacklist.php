e561749cfe91ec76377c66d3fbac87e7346d7bca
x2000habouzit <x2000habouzit>
2004-10-08 11:30:46 +0000

    not used anymore

--- htdocs/admin/postfix_blacklist.php
+++ htdocs/admin/postfix_blacklist.php
@@ -18,36 +18,17 @@
  *  Foundation, Inc.,                                                      *
  *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
  ***************************************************************************
-        $Id: postfix_blacklist.php,v 1.6 2004-09-02 23:33:56 x2000bedo Exp $
+        $Id: postfix_blacklist.php,v 1.7 2004-10-08 11:30:46 x2000habouzit Exp $
  ***************************************************************************/
 
-require("auto.prepend.inc.php");
-new_admin_page('admin/postfix.common.tpl');
+require('auto.prepend.inc.php');
+new_admin_table_editor('postfix_blacklist','email',true);
 
-if(isset($_REQUEST['nomligne'])) {
-    $nomligne = $_REQUEST['nomligne'];
+$editor->assign('title', 'Blacklist de postfix');
+$editor->hide_id();
 
-    if (!empty($_REQUEST['del'])) {
-        exec("/home/web/spam/effacerBlacklist $nomligne {$_SESSION['forlife']}");
-        $page->assign('erreur', "Action: DEL($nomligne)");
-    } else if (!empty($_REQUEST['add'])) {
-        exec("/home/web/spam/ajouterBlacklist ".$nomligne);
-        $page->assign('erreur', "Action: ADD($nomligne)");
-    }
-}
+$editor->describe('email','email',true);
+$editor->describe('reject_text','Texte de rejet',true);
 
-$blacklist = Array();
-$fd = @fopen ("/etc/postfix/spamaccess", "r");
-while ($fd && !feof ($fd)) {
-    $buffer = fgets($fd, 4096);
-    if ($buffer[0]!='#' && (strlen($buffer)>1)) { # FIXME $string[i] is deprecated
-        $blacklist[] = $buffer;
-    }
-}
-@fclose($fd);
-
-$page->assign_by_ref('list',$blacklist);
-$page->assign('title','Bannis de polytechnique.org');
-$page->assign('expl','Ne mettre dans les cases que des emails (aucun espace ni commentaire supplémentaire)');
-$page->run();
+$editor->run();
 ?>
