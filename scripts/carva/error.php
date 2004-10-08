e4185f5341d90f7a8cf39e1ebc3ce3b2525fabcc
web <web>
2004-10-08 19:43:29 +0000

    initial commit (yann)

---
+++ scripts/carva/error.php
@@ -0,0 +1,13 @@
+<?php
+header("HTTP/1.0 404 Not Found");
+?>
+
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>404 Not Found</title>
+</head><body>
+<h1>Not Found</h1>
+The requested URL <?php echo $_SERVER['REQUEST_URI'] ?> was not found on this server.<p>
+<hr>
+<address>Apache Server at www.carva.org Port 80</address>
+</body></html>
