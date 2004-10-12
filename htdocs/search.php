bfdbab56f1487431424127937ecf10ad1a8080b7
x2000habouzit <x2000habouzit>
2004-10-12 17:23:01 +0000

    joan wish

--- htdocs/search.php
+++ htdocs/search.php
@@ -18,7 +18,7 @@
  *  Foundation, Inc.,                                                      *
  *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
  ***************************************************************************
-        $Id: search.php,v 1.17 2004-09-02 23:04:19 x2000bedo Exp $
+        $Id: search.php,v 1.18 2004-10-12 17:23:01 x2000habouzit Exp $
  ***************************************************************************/
 
 require("auto.prepend.inc.php");
@@ -64,29 +64,40 @@ if (array_key_exists('rechercher', $_REQUEST)) {
                        r.matricule,i.matricule_ax,
                        u.nom!="" AS inscrit,
                        IF(u.nom!="",u.nom,i.nom) AS nom,
-                       u.epouse,
+                       u.*, ep.alias AS epouse,
                        IF(u.prenom!="",u.prenom,i.prenom) AS prenom,
                        IF(u.promo!="",u.promo,i.promo) AS promo,
                        i.deces!=0 AS decede,
                        a.alias AS forlife,
-                       u.date,
                        ad0.text AS app0text, ad0.url AS app0url, ai0.type AS app0type,
                        ad1.text AS app1text, ad1.url AS app1url, ai1.type AS app1type,
+		       e.entreprise, es.label AS secteur, ef.label AS fonction,
+		       n.text AS nat,
+		       adr.ville, gp.pays, gr.name AS region,
                        c.uid AS contact
                  FROM  '.(($with_soundex)?'recherche_soundex':'recherche').'      AS r
-           INNER JOIN  identification AS i ON (i.matricule=r.matricule)
-            LEFT JOIN  auth_user_md5  AS u ON (u.matricule=r.matricule)
-            LEFT JOIN  aliases        AS a ON (u.user_id = a.id AND a.type="a_vie")
-            LEFT JOIN  contacts       AS c ON (c.uid='.((array_key_exists('uid',$_SESSION))?$_SESSION['uid']:0).' AND c.contact=u.user_id)
-            LEFT  JOIN applis_ins     AS ai0 ON (u.user_id = ai0.uid AND ai0.ordre = 0)
-            LEFT  JOIN applis_def     AS ad0 ON (ad0.id = ai0.aid)
-            LEFT  JOIN applis_ins     AS ai1 ON (u.user_id = ai1.uid AND ai1.ordre = 1)
-            LEFT  JOIN applis_def     AS ad1 ON (ad1.id = ai1.aid)
+           INNER JOIN  identification AS i   ON (i.matricule=r.matricule)
+           INNER JOIN  auth_user_md5  AS u   ON (u.matricule=r.matricule)
+           INNER JOIN  aliases        AS a   ON (u.user_id = a.id AND a.type="a_vie")
+            LEFT JOIN  aliases        AS ep  ON (u.user_id = a.id AND a.type="epouse")
+            LEFT JOIN  contacts       AS c   ON (c.uid='.((array_key_exists('uid',$_SESSION))?$_SESSION['uid']:0).' AND c.contact=u.user_id)
+            LEFT JOIN  applis_ins     AS ai0 ON (u.user_id = ai0.uid AND ai0.ordre = 0)
+            LEFT JOIN  applis_def     AS ad0 ON (ad0.id = ai0.aid)
+            LEFT JOIN  applis_ins     AS ai1 ON (u.user_id = ai1.uid AND ai1.ordre = 1)
+            LEFT JOIN  applis_def     AS ad1 ON (ad1.id = ai1.aid)
+	    LEFT JOIN  entreprises    AS e   ON (e.entrid = 1 AND e.uid = u.user_id)
+	    LEFT JOIN  emploi_secteur AS es  ON (e.secteur = es.id)
+	    LEFT JOIN  emploi_naf     AS ef  ON (e.fonction = ef.id)
+	    LEFT JOIN  nationalites   AS n   ON (u.nationalite = n.id)
+	    LEFT JOIN  adresses       AS adr ON (u.user_id = adr.uid AND FIND_IN_SET(\'active\', adr.statut))
+	    LEFT JOIN  geoloc_pays    AS gp  ON (adr.pays = gp.a2)
+	    LEFT JOIN  geoloc_region  AS gr  ON (adr.pays = gr.a2 AND adr.region = gr.region)
                 WHERE  '.$fields->get_where_statement().'
              ORDER BY  '.implode(',',array_filter(array($fields->get_order_statement(),'promo DESC,nom,prenom'))).'
                 LIMIT  '.$offset->value.','.$globals->search_results_per_page;
 
     $page->mysql_assign($sql, 'resultats', 'nb_resultats','nb_resultats_total');
+    echo mysql_error();
     
     if (!logged() &&
 	$page->get_template_vars('nb_resultats_total')>$globals->public_max_search_results)
--- templates/search.result.private.tpl
+++ templates/search.result.private.tpl
@@ -17,35 +17,68 @@
  *  Foundation, Inc.,                                                      *
  *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
  ***************************************************************************
-        $Id: search.result.private.tpl,v 1.10 2004-10-12 15:25:32 x2000coic Exp $
+        $Id: search.result.private.tpl,v 1.11 2004-10-12 17:23:02 x2000habouzit Exp $
  ***************************************************************************}
 
 <div class="bits">
   {if $result.inscrit==1}
-    <a href="javascript:x()" onclick="popupWin('fiche.php?user={$result.forlife}', 'toolbar=0,location=0,directories=0,status=0,menubar=0,scrollbars=1,resizable=1,width=800,height=500')">
-      <img src="images/loupe.gif" alt="Afficher les détails" />
-    </a>
-    <a href="vcard.php/{$result.forlife}.vcf?x={$result.forlife}">
-      <img src="images/vcard.png" alt="Afficher la carte de visite" />
-    </a>
-    <a href="mescontacts.php?action={if $result.contact!=""}retirer{else}ajouter{/if}&amp;user={$result.forlife}&amp;mode=normal">
-      <img src="images/{if $result.contact!=""}retirer{else}ajouter{/if}.gif" alt="{if $result.contact!=""}Retirer de{else}Ajouter parmi{/if} mes contacts" />
-    </a>
+  <a href="javascript:x()" onclick="popupWin('fiche.php?user={$result.forlife}', 'toolbar=0,location=0,directories=0,status=0,menubar=0,scrollbars=1,resizable=1,width=800,height=500')">
+    <img src="images/loupe.gif" alt="Afficher les détails" />
+  </a>
+  <a href="vcard.php/{$result.forlife}.vcf?x={$result.forlife}">
+    <img src="images/vcard.png" alt="Afficher la carte de visite" />
+  </a>
+  <a href="mescontacts.php?action={if $result.contact!=""}retirer{else}ajouter{/if}&amp;user={$result.forlife}&amp;mode=normal">
+    <img src="images/{if $result.contact!=""}retirer{else}ajouter{/if}.gif" alt="{if $result.contact!=""}Retirer de{else}Ajouter parmi{/if} mes contacts" />
+  </a>
   {/if}
-  {if $is_admin==1}
-    <a href="javascript:x()" onclick="popWin('http://www.polytechniciens.com/index.php?page=AX_FICHE_ANCIEN&amp;anc_id={$result.matricule_ax}')">
-      AX
-    </a>
-  {/if}
+  {perms level='admin'}
+  <a href="javascript:x()" onclick="popWin('http://www.polytechniciens.com/index.php?page=AX_FICHE_ANCIEN&amp;anc_id={$result.matricule_ax}')">
+    AX
+  </a>
+  {/perms}
   <span class="smaller"><strong>{$result.date|date_format:"%d-%m-%Y"}</strong></span>
 </div>
 {if $result.inscrit!=1}
-  {if $result.decede != 1}
-    <div style="float:right">
-      <a href="javascript:x()" onclick="popWin('marketing/public.php?num={$result.matricule*2-100}')">
-        clique ici si tu connais son adresse email !
-      </a>
-    </div>
-  {/if}
+{if $result.decede != 1}
+<div style="float:right">
+  <a href="javascript:x()" onclick="popWin('marketing/public.php?num={$result.matricule*2-100}')">
+    clique ici si tu connais son adresse email !
+  </a>
+</div>
 {/if}
+{/if}
+  <div class="long">
+    <table cellspacing="0" cellpadding="0">
+      {if $result.nat}
+      <tr>
+        <td class="lt">Nationalité:</td>
+        <td class="rt">{$result.nat}</td>
+      </tr>
+      {/if}
+      {if $result.web}
+      <tr>
+        <td class="lt">Page web:</td>
+        <td class="rt"><a href="{$result.web}">{$result.web}</a></td>
+      </tr>
+      {/if}
+      {if $result.pays || $result.ville || $result.pays}
+      <tr>
+        <td class="lt">Géographie:</td>
+        <td class="rt">{implode sep=", " s1=$result.ville s2=$result.region s3=$result.pays}</td>
+      </tr>
+      {/if}
+      {if $result.entreprise}
+      <tr>
+        <td class="lt">Profession:</td>
+        <td class="rt">
+          {$result.entreprise}
+          {if $result.secteur}( {$result.secteur} ){/if}
+          {if $result.fonction}<br />{$result.fonction} ){/if}
+        </td>
+      </tr>
+      {/if}
+    </table>
+  </div>
+
 {* vim:set et sw=2 sts=2 sws=2: *}
