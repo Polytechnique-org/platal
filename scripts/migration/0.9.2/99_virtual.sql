03bc00d30fc77e537044ff2ba4aa7b3b0e1ee689
x2000habouzit <x2000habouzit>
2004-11-07 19:47:03 +0000

    convert all melix

--- scripts/migration/0.9.2/99_virtual.sql
+++ scripts/migration/0.9.2/99_virtual.sql
@@ -3,3 +3,13 @@ alter table virtual change column alias alias varchar(255) not null;
 alter table virtual add unique (alias) ;
 alter table virtual_redirect change column vid vid int not null auto_increment;
 alter table virtual_redirect add index (redirect);
+
+insert into virtual
+    select null,domain,'user'
+    from   groupex.aliases
+    where  domain like'%@melix.net';
+
+insert into  virtual_redirect
+     select  vid,CONCAT(email,'@m4x.org')
+       from  virtual         AS v
+ inner join  groupex.aliases as d ON(v.alias=d.domain);
