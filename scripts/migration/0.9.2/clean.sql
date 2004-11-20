37daa49c0ce48b6445823875dd3d77ee5b74e535
x2000habouzit <x2000habouzit>
2004-11-20 22:51:32 +0000

    move to upgrade/

--- scripts/migration/0.9.2/clean.sql
+++
@@ -1,3 +0,0 @@
-drop table x4dat.alias_demande;
-drop table x4dat.skin_concept;
-drop table x4dat.nb_recherches;
--- scripts/migration/0.9.3/30_auth_user_md5.sql
+++
@@ -1,2 +0,0 @@
-alter table auth_user_md5 change column perms perms enum('user','admin','non-inscrit','disabled');
-alter table auth_user_md5 change column comment comment varchar(64) not null;
