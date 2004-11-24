728479fc7b3a37b1acab61fbb50efb423622b583
x2000habouzit <x2000habouzit>
2004-11-24 14:24:52 +0000

    install/uninstall banana sql

---
+++ install.d/banana/install.sql
@@ -0,0 +1,3 @@
+alter table auth_user_quick add column banana_last timestamp not null;
+
+-- TODO: have to add forums database here
---
+++ install.d/banana/uninstall.sql
@@ -0,0 +1,3 @@
+alter table auth_user_quick drop column banana_last;
+
+-- TODO: have to drop forums database here
