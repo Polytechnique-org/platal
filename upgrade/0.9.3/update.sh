a79219144f439e27fe766d5ba86c70f8c5180686
Pierre Habouzit (MadCoder <pierre.habouzit@m4x.org>
2004-12-03 11:31:02 +0000

    warnings for next release
    git-archimport-id: opensource@polytechnique.org--2005/platal--mainline--0.9--patch-27

--- upgrade/0.9.3/update.sh
+++ upgrade/0.9.3/update.sh
@@ -16,3 +16,15 @@ do
 done
 
 ###########################################################
+
+echo "BE CAREFUL :
+
+* scripts/ is now essentially in bin/
+  (with for this release, the inner path unchanged)
+  --> have to modify crons
+  --> have to modify xml-rpc path in the daemontools
+
+* carva redirects are still in scripts/ but their path and name have changed.
+  --> the error page is scripts/webredirect_error.php
+  --> the redirect page is scripts/webredirect.php
+  "
