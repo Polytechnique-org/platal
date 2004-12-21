192dc7126216b50b89c355c84bd6d68de84fc156
Pierre Habouzit (MadCoder <pierre.habouzit@m4x.org>
2004-12-21 15:08:50 +0000

    bad path
    git-archimport-id: opensource@polytechnique.org--2005/platal--mainline--0.9--patch-123

--- bin/lists.mhonarc_pipe.sh
+++ bin/lists.mhonarc_pipe.sh
@@ -2,7 +2,7 @@
 
 SPOOL=/var/spool/platal/archives/
 OUTDIR="${SPOOL}/$1/`date +'%Y/%m/'`"
-RCFILE=../../install.d/lists/platal.mrc
+RCFILE=../install.d/lists/platal.mrc
 
 [ -d "$OUTDIR" ] || mkdir -p "$OUTDIR"
 exec mhonarc -add -outdir "$OUTDIR" -rcfile "$RCFILE"
