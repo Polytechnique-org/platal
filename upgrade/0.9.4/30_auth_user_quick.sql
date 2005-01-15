2c65ef953b2ffbaafa6988cff8025c7cf18d2d9f
Pierre Habouzit (MadCoder <pierre.habouzit@m4x.org>
2005-01-15 02:19:36 +0000

    dumb thing to avoid a bug wrt timestamps
    git-archimport-id: opensource@polytechnique.org--2005/platal--mainline--0.9--patch-331

--- upgrade/0.9.4/30_auth_user_quick.sql
+++ upgrade/0.9.4/30_auth_user_quick.sql
@@ -1,3 +1,5 @@
+alter table auth_user_quick  add column foo timestamp after user_id;
+
 alter table auth_user_quick add column emails_alias_pub enum('public', 'private') not null default 'private';
 alter table auth_user_quick add column profile_from_ax INT(1) not null default 0;
 alter table auth_user_quick add column core_mail_fmt enum('html', 'texte') not null default 'html';
