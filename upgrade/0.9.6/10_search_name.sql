4dc70e763d4e79525567a91abdc2ab52d7666635
Pierre Habouzit (MadCoder <pierre.habouzit@m4x.org>
2005-04-12 16:05:18 +0000

    smaller sql index
    git-archimport-id: opensource@polytechnique.org--2005/platal--mainline--0.9--patch-536

--- upgrade/0.9.6/10_search_name.sql
+++ upgrade/0.9.6/10_search_name.sql
@@ -3,8 +3,8 @@ CREATE TABLE `search_name` (
   `uid` int(11) NOT NULL default '0',
   `token` char(255) NOT NULL default '',
   `score` int(11) NOT NULL default '0',
-  PRIMARY KEY (uid, token),
-  INDEX (token)
+  PRIMARY KEY (token,uid),
+  INDEX (uid)
 );
 
 
