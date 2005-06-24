516c21aef6847d9d9c339d3591d5081eea53550d
Pierre Habouzit (MadCoder <pierre.habouzit@m4x.org>
2005-06-24 19:06:15 +0000

    backport
    Patches applied:
    
     * opensource@polytechnique.org--2005/platal--release--0.9.7--patch-10
       fix wrt quick search
    
    
    git-archimport-id: opensource@polytechnique.org--2005/platal--mainline--0.9--patch-747

--- bin/search.rebuild_db.php
+++ bin/search.rebuild_db.php
@@ -26,7 +26,7 @@ require('user.func.inc.php');
 ini_set('memory_limit', "16M");
 $globals->xdb->execute('DELETE FROM search_name');
 
-$res = $globals->xdb->iterRow('SELECT auth_user_md5.user_id, nom, prenom, nom_usage, profile_nick FROM auth_user_md5 INNER JOIN auth_user_quick USING(user_id)');
+$res = $globals->xdb->iterRow('SELECT auth_user_md5.user_id, nom, prenom, nom_usage, profile_nick FROM auth_user_md5 LEFT JOIN auth_user_quick USING(user_id)');
 $i = 0;
 $muls = array(1,1,1,0.2);
 while ($tmp = $res->next()) {
