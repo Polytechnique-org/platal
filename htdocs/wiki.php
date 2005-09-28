bf98922ce7376cd0bb1384958339c3180079eb70
x2001corpet <x2001corpet@839d8a87-29fc-0310-9880-83ba4fa771e5>
2005-09-28 07:59:21 +0000

    wiki is coming...
    git-svn-id: svn+ssh://murphy/home/svn/platal/trunk@65 839d8a87-29fc-0310-9880-83ba4fa771e5

---
+++ htdocs/wiki.php
@@ -0,0 +1,44 @@
+<?php
+/***************************************************************************
+ *  Copyright (C) 2003-2004 Polytechnique.org                              *
+ *  http://opensource.polytechnique.org/                                   *
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program; if not, write to the Free Software            *
+ *  Foundation, Inc.,                                                      *
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
+ ***************************************************************************/
+
+require_once("xorg.inc.php");
+if (Env::has('response'))
+ new_skinned_page('wiki.tpl', AUTH_MDP);
+else
+ new_skinned_page('wiki.tpl', AUTH_PUBLIC);
+
+if ($globals->wiki->wikidir)
+{
+ ob_start();
+ require_once($globals->spoolroot.$globals->wiki->wikidir.'/pmwiki.php');
+
+ $wikiAll = ob_get_clean();
+ $i = strpos($wikiAll, "<!--/HeaderText-->");
+
+ $page->assign('xorg_extra_header', substr($wikiAll, 0, $i));
+
+ $wikiTxt = substr($wikiAll, $i);
+
+ $page->assign('pmwiki', $wikiTxt);
+}
+
+$page->run();
+?>
