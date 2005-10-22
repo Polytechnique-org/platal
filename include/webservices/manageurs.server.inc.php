9e58f667abdaacefdee78ab961e3a4fd78dea0ac
x2002marichez <x2002marichez@839d8a87-29fc-0310-9880-83ba4fa771e5>
2005-10-22 18:40:00 +0000

    Webservice Manageurs pas à jour
    git-svn-id: svn+ssh://murphy/home/svn/platal/branches/platal-0.9.8@133 839d8a87-29fc-0310-9880-83ba4fa771e5

--- include/webservices/manageurs.server.inc.php
+++ include/webservices/manageurs.server.inc.php
@@ -4,20 +4,23 @@ require_once('webservices/manageurs.inc.php');
 
 $error_mat = "You didn't provide me with a valid matricule number...";
 $error_key = "You didn't provide me with a valid cipher key...";
-
 /**
   le premier parametre doit etre le matricule
   le second parametre facultatif doit etre le numero de l'adresse voulue :
     -1 => on ne veut pas d'adresse
     0 => on veut toutes les adresses
     n => on veut l'adresse numero n
+
+    IL NE FAUT PAS CHANGER LES NOMS DES CHAMPS DE ADRESSES
+    S'IL Y A DES MODIFS A FAIRE VOIR AVEC MANAGEURS admin@manageurs.com
 */
 function get_annuaire_infos($method, $params) {
     global $error_mat, $error_key, $globals;
 
-    //verif du mdp
-    if(!isset($params[0]) || ($params[0] != $globals->manageurs->pass)){return false;}
+      
 
+    //verif du mdp
+    if(!isset($params[0]) || ($params[0] != $globals->manageurs->manageurs_pass)){return false;}
     //si on a adresse == -1 => on ne recupère aucune adresse
     if(isset($params[2]) && ($params[2] == -1)) unset($params[2]);
 
@@ -28,9 +31,9 @@ function get_annuaire_infos($method, $params) {
         if(!isset($params[2])){
             $res = $globals->xdb->iterRow(
                     "SELECT  q.profile_mobile AS cell, a.naissance AS age
-                       FROM  auth_user_md5 AS a
-		       INNER JOIN auth_user_quick AS q USING (user_id)
-                      WHERE  a.matricule = {?}", $params[1]);
+                    FROM  auth_user_md5 AS a
+                    INNER JOIN auth_user_quick AS q USING (user_id)
+                    WHERE  a.matricule = {?}", $params[1]);
         }
         else{
             $res = $globals->xdb->iterRow(
@@ -47,12 +50,12 @@ function get_annuaire_infos($method, $params) {
                              FIND_IN_SET('res-secondaire', adr.statut),
                              NOT FIND_IN_SET('courrier', adr.statut)", $params[1]);
 
-                       }
+                    }
 
-        //traitement des adresss si necessaire
+        //traitement des adresses si necessaire
         if (isset($params[2])) {
-            if(list($cell, $age, $adr['adr1'], $adr['adr2'], $adr['adr3'], $adr['postcode'], $adr['city'],
-                        $adr['country'], $adr['tel'], $adr['fax']) = $res->next())
+            if(list($cell, $age, $adr['adr1'], $adr['adr2'], $adr['adr3'], $adr['cp'], $adr['ville'],
+                        $adr['pays'], $adr['tel'], $adr['fax']) = $res->next())
             {
                 $array['cell']      = $cell;
                 $array['age']       = $age;
@@ -63,8 +66,8 @@ function get_annuaire_infos($method, $params) {
 
                 if ($adresse != 1) { //on ne veut pas la premiere adresse
                     $i = 2;
-                    while(list($cell, $age, $adr['adr1'], $adr['adr2'], $adr['adr3'], $adr['postcode'], $adr['city'],
-                                $adr['country'], $adr['tel'], $adr['fax']) = $res->next())
+                    while(list($cell, $age, $adr['adr1'], $adr['adr2'], $adr['adr3'], $adr['cp'], $adr['ville'],
+                                $adr['pays'], $adr['tel'], $adr['fax']) = $res->next())
                     {
                         if($adresse == $i){//si on veut cette adresse en particulier
                             $array['adresse'][0] = $adr;
@@ -85,6 +88,7 @@ function get_annuaire_infos($method, $params) {
         else { //cas où on ne veut pas d'adresse
             $array = $res->next();
         }
+        
 
         if ($array) { // on a bien eu un résultat : le matricule etait bon
 
@@ -115,8 +119,41 @@ function get_annuaire_infos($method, $params) {
     } else {//le matricule n'etait pas en argument
         $args  = array("erreur" => 1, "erreurstring" => $error_mat);
         $reply = xmlrpc_encode_request(NULL,$args);
-    } 
+    }
+            
     return $reply; 
 } 
+
+function get_nouveau_infos($method, $params) {
+    global $error_mat, $error_key, $globals;
+    //verif du mdp
+    if(!isset($params[0]) || ($params[0] != $globals->manageurs->manageurs_pass)){return false;}
+    if( !empty($params[1]) ){ // on verifie qu'on a bien un matricule
+
+        $res = $globals->xdb->query(
+                "SELECT  a.nom, a.nom_usage,a.prenom,a.flags='femme' as femme ,a.deces!= 0 as decede ,a.naissance,a.promo,al.alias as mail 
+                FROM  auth_user_md5 AS a
+                INNER JOIN aliases as al ON a.user_id=al.id
+                WHERE al.flags='bestalias' and  a.matricule = {?}",$params[1]);
+        $data=$res->fetchOneAssoc();
+        //$data['mail'].='@polytechnique.org';
+
+        
+        //on commence le cryptage des donnees
+        if (manageurs_encrypt_init($params[1]) == 1) {//on a pas trouve la cle pour crypter
+            $args  = array("erreur" => 3, "erreurstring" => $error_key);
+            $reply = xmlrpc_encode_request(NULL,$args);
+        } else {
+            $reply = manageurs_encrypt_array($data);
+            manageurs_encrypt_close();
+        }
+
+    }
+    else{
+    $reply=false;
+    }
+        return $reply;
+
+}
 
 ?>
