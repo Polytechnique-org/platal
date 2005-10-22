8ac323defbbd60733f978c563b335677a79a988c
x2000habouzit <x2000habouzit@839d8a87-29fc-0310-9880-83ba4fa771e5>
2005-10-22 12:25:28 +0000

    normalize code a bit, delete unused comments for options, since it slowdown parse. enable browser cache
    git-svn-id: svn+ssh://murphy/home/svn/platal/trunk@130 839d8a87-29fc-0310-9880-83ba4fa771e5

--- plugins/pmwiki.platalSkin.php
+++ plugins/pmwiki.platalSkin.php
@@ -4,11 +4,13 @@
 $Author = $_SESSION['forlife']."|".$_SESSION['prenom']." ".$_SESSION['nom'];
 
 // set profiles to point to plat/al fiche
-Markup("[[~platal", "<[[~", '/\[\[~([^|\]]*)\|([^\]]*)\]\]/e', 'PreserveText("=", \'<a href="'.$globals->baseurl.'/fiche.php?user=$1" class="popup2">$2</a>\', "")');
+Markup('[[~platal', '<[[~', '/\[\[~([^|\]]*)\|([^\]]*)\]\]/e',
+    'PreserveText("=", \'<a href="'.$globals->baseurl
+    .'/fiche.php?user=$1" class="popup2">$2</a>\', "")');
 
 // add conditions for request vars
 // (:request topic:) then (:if topic top:) will test $_REQUEST['topic'] == 'top'
-Markup("requestVars", "<if", '/\(:request ([a-z_]+)( ([a-z_]+))?:\)/ie', 'RequestToCond("$1", "$3")');
+Markup('requestVars', '<if', '/\(:request ([a-z_]+)( ([a-z_]+))?:\)/ie', 'RequestToCond("$1", "$3")');
 
 ## [[#anchor]] in standard XHTML
 Markup('[[#','<[[','/(?>\\[\\[#([A-Za-z][-.:\\w]*))\\]\\]/e',
@@ -16,26 +18,31 @@ Markup('[[#','<[[','/(?>\\[\\[#([A-Za-z][-.:\\w]*))\\]\\]/e',
   
 function RequestToCond($var, $default)
 {
- global $Conditions;
- $Conditions[$var] = '(!$condparm && !isset($_REQUEST["'.$var.'"])) || $_REQUEST["'.$var.'"]==$condparm';
- if ($default)
-  $Conditions[$var] .= ' || "'.$default.'" == $_REQUEST["'.$var.'"]';
+    global $Conditions;
+    $Conditions[$var] = '(!$condparm && !isset($_REQUEST["'.$var
+        .'"])) || $_REQUEST["'.$var.'"]==$condparm';
+    if ($default) {
+        $Conditions[$var] .= ' || "'.$default.'" == $_REQUEST["'.$var.'"]';
+    }
 }
 
 // add access to session vars
-Markup("sessionvars", "inline", '/\(:session ([a-z_]+):\)/ie', 'Session::get("$1")');
+Markup('sessionvars', 'inline', '/\(:session ([a-z_]+):\)/ie', 'Session::get("$1")');
 
-Markup("tablebicol", "<block", '/\(:tablebicol ?([a-z_]+)?:\)/e', 'doBicol("$1")');
-Markup("pairrows", "_end", '/class=\'pair\_pmwiki\_([0-9]+)\'/e', "($1 == 1)?'':('class=\"'.(($1 % 2 == 0)?'impair':'pair').'\"')");
-Markup("noclassth", "_end", '/<th class=\'[a-z_]+\'/', "<th");
+Markup('tablebicol', '<block', '/\(:tablebicol ?([a-z_]+)?:\)/e', 'doBicol("$1")');
+Markup('pairrows', '_end', '/class=\'pair\_pmwiki\_([0-9]+)\'/e', 
+    "($1 == 1)?'':('class=\"'.(($1 % 2 == 0)?'impair':'pair').'\"')");
+Markup('noclassth', '_end', '/<th class=\'[a-z_]+\'/', '<th');
 
-Markup("div", "<links", '/\(:div([^:]*):([^\)]*):\)/i', "<div$1>$2</div>");
+Markup('div', '<links', '/\(:div([^:]*):([^\)]*):\)/i', '<div$1>$2</div>');
 
 function doBicol($column=false)
 {
- global $TableRowIndexMax, $TableRowAttrFmt, $TableCellAttrFmt;
- $TableRowAttrFmt = "class='pair_pmwiki_\$TableRowCount'";
- if ($column)
-  $TableCellAttrFmt = "class='$column'";
+    global $TableRowIndexMax, $TableRowAttrFmt, $TableCellAttrFmt;
+    $TableRowAttrFmt = "class='pair_pmwiki_\$TableRowCount'";
+    if ($column) {
+        $TableCellAttrFmt = "class='$column'";
+    }
 }
 
+?>
