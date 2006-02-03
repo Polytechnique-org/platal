c1e60d2053a19af0198d644baa5dc337580eb395
x2002bobillot <x2002bobillot@839d8a87-29fc-0310-9880-83ba4fa771e5>
2006-02-03 23:50:48 +0000

    backport bugfix paiement
    git-svn-id: svn+ssh://murphy/home/svn/platal/trunk@223 839d8a87-29fc-0310-9880-83ba4fa771e5

--- htdocs/index.php
+++ htdocs/index.php
@@ -18,7 +18,6 @@
  *  Foundation, Inc.,                                                      *
  *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
  ***************************************************************************/
-
 require_once("xorg.inc.php");
 new_skinned_page('index.tpl', AUTH_PUBLIC);
 if (logged()) {
