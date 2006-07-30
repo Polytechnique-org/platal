b406e3547d673579dd9f75bc98c6a30180d257ef
x2003bruneau <x2003bruneau@839d8a87-29fc-0310-9880-83ba4fa771e5>
2006-07-30 10:18:40 +0000

    Fix text encoding
    git-svn-id: svn+ssh://murphy/home/svn/platal/trunk@707 839d8a87-29fc-0310-9880-83ba4fa771e5

--- classes/Session.php
+++ classes/Session.php
@@ -92,7 +92,7 @@ function check_perms()
         if ($_SESSION['log']) {
             $_SESSION['log']->log("noperms",$_SERVER['PHP_SELF']);
         }
-	$page->kill("Tu n'as pas les permissions nÃ©cessaires pour accÃ©der Ã  cette page.");
+	$page->kill("Tu n'as pas les permissions nécessaires pour accéder à cette page.");
     }
 }
 
