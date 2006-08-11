343fce72840fc79d4399cc4d74cfa4487663278c
x2003bruneau <x2003bruneau@839d8a87-29fc-0310-9880-83ba4fa771e5>
2006-08-11 23:29:39 +0000

    Date column should be of DATE type...
    git-svn-id: svn+ssh://murphy/home/svn/platal/trunk@805 839d8a87-29fc-0310-9880-83ba4fa771e5

--- upgrade/0.9.11/08_skins.sql
+++ upgrade/0.9.11/08_skins.sql
@@ -1,3 +1,4 @@
+alter table skins change date date date not null default 0; 
 INSERT INTO skins (id, name, date, comment, auteur, skin_tpl, ext)
      VALUES (13, 'Keynote', '2006-08-09', 'Skin inspirée d\'un thème de Keynote',
              'Florent Bruneau', 'keynote.tpl', 'png');
