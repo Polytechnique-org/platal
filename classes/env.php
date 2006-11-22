ae0832b8464b182ce1fe87c05f3a538a0bb4a8c5
x2003bruneau <x2003bruneau@839d8a87-29fc-0310-9880-83ba4fa771e5>
2006-11-22 09:26:45 +0000

    #545: Fix windows special chars encoding issues
    git-svn-id: svn+ssh://murphy/home/svn/platal/trunk@1125 839d8a87-29fc-0310-9880-83ba4fa771e5

--- classes/env.php
+++ classes/env.php
@@ -162,6 +162,13 @@ function fix_gpc_magic(&$item, $key) {
         $item = stripslashes($item);
     }
 }
+function fix_encoding(&$item, $key) {
+    if (is_array($item)) {
+        array_walk($item, 'fix_encoding');
+    } elseif (preg_match('/[\x80-\x9f]/', $item)) {
+        $item = iconv('CP1252', 'ISO-8859-15//TRANSLIT', $item);
+    }
+}
 
 if (ini_get('magic_quotes_gpc') && empty($DONT_FIX_GPC)) {
     array_walk($_GET, 'fix_gpc_magic');
@@ -169,6 +176,7 @@ if (ini_get('magic_quotes_gpc') && empty($DONT_FIX_GPC)) {
     array_walk($_COOKIE, 'fix_gpc_magic');
     array_walk($_REQUEST, 'fix_gpc_magic');
 }
+array_walk($_POST, 'fix_encoding');
 
 // vim:set et sw=4 sts=4 sws=4 foldmethod=marker:
 ?>
