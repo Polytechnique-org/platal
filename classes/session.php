5993d0c7fb6b905ea179add9197d500adb1b278a
x2003bruneau <x2003bruneau@839d8a87-29fc-0310-9880-83ba4fa771e5>
2006-11-27 11:12:45 +0000

    Backport (close #566)
    git-svn-id: svn+ssh://murphy/home/svn/platal/trunk@1183 839d8a87-29fc-0310-9880-83ba4fa771e5

--- classes/session.php
+++ classes/session.php
@@ -19,6 +19,7 @@
  *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
  ***************************************************************************/
 
+global $lc_accent, $lc_plain, $uc_accent, $uc_plain;
 require_once 'diogenes/diogenes.misc.inc.php';
 
 class Session
