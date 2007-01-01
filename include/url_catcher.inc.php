21a41057ab5a093dcf570f0feff4e2abb272f1cf
x2000habouzit <x2000habouzit@839d8a87-29fc-0310-9880-83ba4fa771e5>
2007-01-01 14:48:51 +0000

    merge
    git-svn-id: svn+ssh://murphy/home/svn/platal/trunk@1322 839d8a87-29fc-0310-9880-83ba4fa771e5

--- include/url_catcher.inc.php
+++ include/url_catcher.inc.php
@@ -3,22 +3,22 @@
 // renvoie un texte html à partir d'un texte classique
 // en remplacant les url par des liens (éventuellement les mails)
 function url_catcher($texte, $mails = true) {
-	$patterns = array();
-	$replacement = array();
-	
-	// url commencant par http, https ou ftp
-	$patterns[] = '/((?:https?|ftp):\/\/(?:\.*,*[a-z@0-9~%$£µ&i#\-+=_\/\?])*)/i';
-	$replacement[] = '<a href="\\0">\\0</a>';
-	
-	// url commencant par www.
-	$patterns[] = '/(\s|^)www\.((?:\.*,*[a-z@0-9~%$£µ&i#\-+=_\/\?])*)/i';
-	$replacement[] = '\\1<a href="http://www.\\2">www.\\2</a>';
-	
-	if ($mails) {
+    $patterns = array();
+    $replacement = array();
+
+    // url commencant par http, https ou ftp
+    $patterns[] = '/((?:https?|ftp):\/\/(?:\.*,*[a-z@0-9~%$£µ&i#\-+=_\/\?;])*)/i';
+    $replacement[] = '<a href="\\0">\\0</a>';
+
+    // url commencant par www.
+    $patterns[] = '/(\s|^)www\.((?:\.*,*[a-z@0-9~%$£µ&i#\-+=_\/\?;])*)/i';
+    $replacement[] = '\\1<a href="http://www.\\2">www.\\2</a>';
+
+    if ($mails) {
         $patterns[] = '/(?:mailto:)?([a-z0-9.\-+_]+@([\-.+_]?[a-z0-9])+)/i';
         $replacement[] = '<a href="mailto:\\0">\\0</a>';
-	}
-	
+    }
+
     return preg_replace($patterns, $replacement, $texte);
 }
 ?>
