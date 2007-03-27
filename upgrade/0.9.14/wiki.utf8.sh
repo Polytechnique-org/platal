73cf81b8b1acba2b3056f789c68a40fff90efd04
x2003bruneau <x2003bruneau@839d8a87-29fc-0310-9880-83ba4fa771e5>
2007-03-27 21:48:11 +0000

    Convert image name to UTF8
    git-svn-id: svn+ssh://murphy/home/svn/platal/trunk@1625 839d8a87-29fc-0310-9880-83ba4fa771e5

--- upgrade/0.9.14/wiki.utf8.sh
+++ upgrade/0.9.14/wiki.utf8.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
 WIKISPOOLDIR='../../spool/wiki.d/'
+IMAGESPOOLDIR='../../spool/uploads/'
 
 find $WIKISPOOLDIR -name 'cache_*' -or -name 'tmp_*' -exec rm {} ";"
 for i in `find $WIKISPOOLDIR -type f`; do
     CONV=`echo -n $i | iconv -t UTF-8`
     mv $i $i.latin1
     iconv -t UTF-8 $i.latin1 > $CONV
+done
+
+for i in `find $IMAGESPOOLDIR -type f`; do
+    CONV=`echo -n $i | iconv -t UTF-8`
+    if [ $i != $CONV ]; then
+        mv $i $CONV
+    fi
 done
 
 echo "Les pages de wiki ont ete converites en UTF-8"
