4e4b828bf5ac191c5c94a7f9c073e59b7b7c6fc1
Florent Bruneau <florent.bruneau@polytechnique.org>
2007-11-15 13:03:26 +0100

    Do not store payments on multiple SQL entry.
    Signed-off-by: Florent Bruneau <florent.bruneau@polytechnique.org>

--- modules/xnetevents.php
+++ modules/xnetevents.php
@@ -524,7 +524,7 @@ class XnetEventsModule extends PLModule
             if (Env::v('adm') == 'prix' && $member) {
                 XDB::execute("UPDATE groupex.evenements_participants
                                  SET paid = IF(paid + {?} > 0, paid + {?}, 0)
-                               WHERE uid = {?} AND eid = {?}",
+                               WHERE uid = {?} AND eid = {?} AND item_id = 1",
                         strtr(Env::v('montant'), ',', '.'),
                         strtr(Env::v('montant'), ',', '.'),
                         $member['uid'], $evt['eid']);
