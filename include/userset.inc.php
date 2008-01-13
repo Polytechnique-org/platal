9d66aa4a9c146715c3d50eb7e5817193f0978b17
Florent Bruneau <florent.bruneau@polytechnique.org>
2008-01-13 23:17:05 +0100

    Fix a possible SQL error.
    Signed-off-by: Florent Bruneau <florent.bruneau@polytechnique.org>

--- include/userset.inc.php
+++ include/userset.inc.php
@@ -48,7 +48,7 @@ class UserSet extends PlSet
                                 'INNER JOIN groupex.membres AS gxm ON (u.user_id = gxm.uid
                                                                        AND gxm.asso_id = ' . $globals->asso('id') . ') ' : '')
                            . 'LEFT JOIN auth_user_quick AS q USING (user_id)
-                              LEFT JOIN aliases         AS a ON (a.id = u.user_id AND type = \'a_vie\')
+                              LEFT JOIN aliases         AS a ON (a.id = u.user_id AND a.type = \'a_vie\')
                               ' . $joins,
                             $where,
                             'u.user_id');
