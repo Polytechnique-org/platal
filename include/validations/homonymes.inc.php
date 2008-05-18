Polytechnique-org/platal

commit d7dd70be3dd8f76ae6dc9fed7977e7aa7b603085

Remove most references to @polytechnique.org email adresses.
git-svn-id: svn+ssh://murphy/home/svn/platal/trunk@1897 839d8a87-29fc-0310-9880-83ba4fa771e5

include/validations/homonymes.inc.php
@@ -69,7 +69,7 @@ class HomonymeReq extends Validate
 
     protected function _mail_subj()
     {
-        return "[Polytechnique.org/Support] ".($this->warning?"Dans une semaine : suppression de l'alias":"Mise en place du robot")." $loginbis@polytechnique.org";
+        return "[Polytechnique.org/Support] ".($this->warning?"Dans une semaine : suppression de l'alias":"Mise en place du robot")." $loginbis@" . $globals->mails->domain;
     }
 
     // }}}

modules/axletter.php
@@ -159,7 +159,7 @@ class AXLetterModule extends PLModule
                              $id, $shortname, $subject, $title, $body, $signature, $promo_min, $promo_max, $echeance);
                 if (!$saved) {
                     $mailer = new PlMailer();
-                    $mailer->setFrom("support@polytechnique.org");
+                    $mailer->setFrom("support@" . $globals->mails->domain);
                     $mailer->setSubject("Un nouveau projet de mail de l'AX vient d'être proposé");
                     $mailer->setTxtBody("Un nouveau mail vient d'être rédigé en prévision d'un envoi prochain. Vous pouvez "
                                       . "le modifier jusqu'à ce qu'il soit verrouillé pour l'envoi\n\n"

modules/marketing.php
@@ -118,7 +118,7 @@ class MarketingModule extends PLModule
                 $from  = $market->sender_mail;
                 $page->assign('rel_from_user', $from);
                 $page->assign('rel_from_staff',
-                              '"Equipe Polytechnique.org" <register@polytechnique.org>');
+                              '"Equipe Polytechnique.org" <register@' . $globals->mails->domain . '>');
                 $page->assign('rel_to', $to);
                 $page->assign('rel_title', $title);
                 $page->assign('rel_text', $text);

modules/payment.php
@@ -22,8 +22,8 @@
 /* sort en affichant une erreur */
 function cb_erreur($text) {
     $mymail = new PlMailer();
-    $mymail->addTo("telepaiement@polytechnique.org");
-    $mymail->setFrom("webmaster@polytechnique.org");
+    $mymail->addTo($globals->money->email);
+    $mymail->setFrom("webmaster@" . $globals->mails->domain);
     $mymail->setSubject("erreur lors d'un télépaiement (CyberPaiement)");
     $mymail->setTxtBody("\n\n".var_export($_REQUEST,true));
     $mymail->send();
@@ -39,8 +39,8 @@ function paypal_erreur($text, $send=true)
     if (!$send) return;
 
     $mymail = new PlMailer();
-    $mymail->addTo("telepaiement@polytechnique.org");
-    $mymail->setFrom("webmaster@polytechnique.org");
+    $mymail->addTo($globals->money->email);
+    $mymail->setFrom("webmaster@" . $globals->mails->domain);
     $mymail->setSubject("erreur lors d'un télépaiement (PayPal)");
     $mymail->setTxtBody("\n\n".var_export($_REQUEST,true));
     $mymail->send();
@@ -220,7 +220,7 @@ class PaymentModule extends PLModule
 
         $mymail = new PlMailer();
         $mymail->setFrom($conf_mail);
-        $mymail->addTo("\"$prenom $nom\" <$forlife@polytechnique.org>");
+        $mymail->addTo("\"$prenom $nom\" <$forlife@" . $globals->mails->domain . '>');
         $mymail->addCc($conf_mail);
         $mymail->setSubject($conf_title);
         $mymail->setWikiBody($conf_text);
@@ -228,8 +228,8 @@ class PaymentModule extends PLModule
 
         /* on envoie les details de la transaction à telepaiement@ */
         $mymail = new PlMailer();
-        $mymail->setFrom("webmaster@polytechnique.org");
-        $mymail->addTo("telepaiement@staff.polytechnique.org");
+        $mymail->setFrom("webmaster@" . $globals->mails->domain);
+        $mymail->addTo($globals->money->email);
         $mymail->setSubject($conf_title);
         $msg = "utilisateur : $prenom $nom ($uid)\n".
                "mail : $forlife@polytechnique.org\n\n".
@@ -309,7 +309,7 @@ class PaymentModule extends PLModule
 
         $mymail = new PlMailer();
         $mymail->setFrom($conf_mail);
-        $mymail->addTo("\"$prenom $nom\" <$forlife@polytechnique.org>");
+        $mymail->addTo("\"$prenom $nom\" <$forlife@" . $globals->mails->domain . '>');
         $mymail->addCc($conf_mail);
         $mymail->setSubject($conf_title);
         $mymail->setWikiBody($conf_text);
@@ -317,8 +317,8 @@ class PaymentModule extends PLModule
 
         /* on envoie les details de la transaction à telepaiement@ */
         $mymail = new PlMailer();
-        $mymail->setFrom("webmaster@polytechnique.org");
-        $mymail->addTo("telepaiement@polytechnique.org");
+        $mymail->setFrom("webmaster@" . $globals->mails->domain);
+        $mymail->addTo($globals->money->email);
         $mymail->setSubject($conf_title);
         $msg = "utilisateur : $prenom $nom ($uid)\n".
                "mail : $forlife@polytechnique.org\n\n".

modules/register.php
@@ -192,7 +192,8 @@ class RegisterModule extends PLModule
                         $sub_state['email']     = Post::v('email');
                         if (check_ip('unsafe')) {
                             $err = "Une erreur s'est produite lors de l'inscription."
-                                 . " Merci de contacter <a href='mailto:register@polytechnique.org'>register@polytechnique.org</a>"
+                                 . " Merci de contacter <a href='mailto:register@{$globals->mails->domain}>"
+                                 . " register@{$globals->mails->domain}</a>"
                                  . " pour nous faire part de cette erreur";
                             $alert .= "Tentative d'inscription depuis une IP surveillee";
                         } else {
@@ -328,7 +329,7 @@ class RegisterModule extends PLModule
         while (list($salias, $snom, $sprenom, $sfemme) = $res->next()) {
             $mymail = new PlMailer();
             $mymail->setSubject("$prenom $nom s'est inscrit à Polytechnique.org !");
-            $mymail->setFrom('"Marketing Polytechnique.org" <register@polytechnique.org>');
+            $mymail->setFrom('"Marketing Polytechnique.org" <register@' . $globals->mails->domain . '>');
             $mymail->addTo("\"$sprenom $snom\" <$salias@{$globals->mail->domain}>");
             $msg = ($sfemme?'Chère':'Cher')." $sprenom,\n\n"
                  . "Nous t'écrivons pour t'informer que $prenom $nom (X$promo), "
@@ -345,7 +346,7 @@ class RegisterModule extends PLModule
         if ($globals->register->notif) {
             $mymail = new PlMailer();
             $mymail->setSubject("Inscription de $prenom $nom (X$promo)");
-            $mymail->setFrom('"Webmaster Polytechnique.org" <web@polytechnique.org>');
+            $mymail->setFrom('"Webmaster Polytechnique.org" <web@' . $globals->mails->domain . '>');
             $mymail->addTo($globals->register->notif);
             $msg = "$prenom $nom (X$promo) a terminé son inscription avec les données suivantes :\n"
                  . " - nom       : $nom\n"

modules/register/register.inc.php
@@ -158,7 +158,8 @@ function create_aliases (&$sub)
     $res      = XDB::query('SELECT COUNT(*) FROM aliases WHERE alias={?}', $forlife);
     if ($res->fetchOneCell() > 0) {
         return "Tu as un homonyme dans ta promo, il faut traiter ce cas manuellement.<br />".
-            "envoie un mail à <a href=\"mailto:support@polytechnique.org\">support@polytechnique.org</a> en expliquant ta situation.";
+            "envoie un mail à <a href=\"mailto:support@{$globals->mail->domain}</a>\">" .
+            "support@{$globals->mail->domain}</a> en expliquant ta situation.";
     }
     
     $res      = XDB::query('SELECT id, type, expire FROM aliases WHERE alias={?}', $mailorg);
@@ -175,10 +176,10 @@ function create_aliases (&$sub)
             $als = $res->fetchColumn();
 
             $mailer = new PlMailer();
-            $mailer->setFrom('"Support Polytechnique.org" <support@polytechnique.org>');
-            $mailer->addTo("$mailorg@polytechnique.org");
+            $mailer->setFrom('"Support Polytechnique.org" <support@' . $globals->mails->domain . '>');
+            $mailer->addTo("$mailorg@" . $globals->mails->domain);
             $mailer->setSubject("perte de ton alias $mailorg dans un mois !");
-            $mailer->addCc('"Support Polytechnique.org" <support@polytechnique.org>');
+            $mailer->addCc('"Support Polytechnique.org" <support@' . $globals->mails->domain . '>');
             $msg =
                 "Bonjour,\n\n".
                 
@@ -245,7 +246,7 @@ function finish_ins($sub_state)
     $mymail->assign('pass',    $pass);
     $mymail->assign('baseurl', $globals->baseurl);
     $mymail->assign('hash',    $hash);
-    $mymail->assign('subj',    $bestalias."@polytechnique.org");
+    $mymail->assign('subj',    $bestalias."@" . $globals->mails->domain);
     $mymail->send();
 }
 

include/homonymes.inc.php
@@ -29,24 +29,24 @@ function select_if_homonyme($uid) {
 }
 
 function send_warning_homonyme($prenom, $nom, $forlife, $loginbis) {
-    $cc = "support+homonyme@polytechnique.org";
+    $cc = "support+homonyme@" . $globals->mails->domain;
     $FROM = "\"Support Polytechnique.org\" <$cc>";
     $mymail = new PlMailer();
     $mymail->setFrom($FROM);
-    $mymail->setSubject("Dans 2 semaines, suppression de $loginbis@polytechnique.org");
-    $mymail->addTo("$prenom $nom <$forlife@polytechnique.org>");
+    $mymail->setSubject("Dans 2 semaines, suppression de $loginbis@" . $globals->mails->domain);
+    $mymail->addTo("$prenom $nom <$forlife@" . $globals->mails->domain . '>');
     $mymail->addCc($cc);
     $mymail->setTxtBody(Env::v('mailbody'));
     $mymail->send();
 }
 
 function send_robot_homonyme($prenom, $nom, $forlife, $loginbis) {
-    $cc = "support+homonyme@polytechnique.org";
+    $cc = "support+homonyme@" . $globals->mails->domain;
     $FROM = "\"Support Polytechnique.org\" <$cc>";
     $mymail = new PlMailer();
     $mymail->setFrom($FROM);
-    $mymail->setSubject("Mise en place du robot $loginbis@polytechnique.org");
-    $mymail->addTo("$prenom $nom <$forlife@polytechnique.org>");
+    $mymail->setSubject("Mise en place du robot $loginbis@" . $globals->mails->domain);
+    $mymail->addTo("$prenom $nom <$forlife@" . $globals->mails->domain . '>');
     $mymail->addCc($cc);
     $mymail->setTxtBody(Env::v('mailbody'));
     $mymail->send();

bin/cron/cron_validations.php
@@ -39,8 +39,8 @@ if (empty($nb)) {
 $plural = $nb == 1 ? "" : "s";
 
 $mymail = new PlMailer();
-$mymail->setFrom('validation@polytechnique.org');
-$mymail->addTo("validation@polytechnique.org");
+$mymail->setFrom('validation@' . $globals->mails->domain);
+$mymail->addTo("validation@" . $globals->mails->domain);
 $mymail->setSubject((empty($nbveryold)?"":"[urgent] ")."il y a $nb validation$plural non effectuée$plural");
 
 $message =

commit 115c90dbde9227ff714f699c546f93c108f385f3

Adds missing "global $globals", used for sending homonyms/payment errors/registration/marketing emails (bugs introduced in r1897).
Signed-off-by: Vincent Zanotti <vincent.zanotti@polytechnique.org>

include/validations/homonymes.inc.php
@@ -69,6 +69,7 @@ class HomonymeReq extends Validate
 
     protected function _mail_subj()
     {
+        global $globals;
         return "[Polytechnique.org/Support] ".($this->warning?"Dans une semaine : suppression de l'alias":"Mise en place du robot")." $loginbis@" . $globals->mail->domain;
     }
 

modules/axletter.php
@@ -158,6 +158,7 @@ class AXLetterModule extends PLModule
                                             signature = {?}, promo_min = {?}, promo_max = {?}, echeance = {?}",
                              $id, $shortname, $subject, $title, $body, $signature, $promo_min, $promo_max, $echeance);
                 if (!$saved) {
+                    global $globals;
                     $mailer = new PlMailer();
                     $mailer->setFrom("support@" . $globals->mail->domain);
                     $mailer->setSubject("Un nouveau projet de mail de l'AX vient d'être proposé");
@@ -177,7 +178,6 @@ class AXLetterModule extends PLModule
                                      INNER JOIN auth_user_md5   AS u USING(user_id)
                                      INNER JOIN aliases         AS a ON (u.user_id = a.id
                                      AND FIND_IN_SET('bestalias', a.flags))");
-                    global $globals;
                     while (list($nom, $prenom, $alias) = $res->next()) {
                         $mailer->addTo("$nom $prenom <$alias@{$globals->mail->domain}>");
                     }

modules/marketing.php
@@ -79,6 +79,7 @@ class MarketingModule extends PLModule
     function handler_private(&$page, $uid = null,
                              $action = null, $value = null)
     {
+        global $globals;
         $page->changeTpl('marketing/private.tpl');
 
         if (is_null($uid)) {

modules/payment.php
@@ -21,6 +21,7 @@
 
 /* sort en affichant une erreur */
 function cb_erreur($text) {
+    global $globals;
     $mymail = new PlMailer();
     $mymail->addTo($globals->money->email);
     $mymail->setFrom("webmaster@" . $globals->mail->domain);
@@ -33,7 +34,7 @@ function cb_erreur($text) {
 /* sort en affichant une erreur */
 function paypal_erreur($text, $send=true)
 {
-    global $page, $erreur;
+    global $page, $erreur, $globals;
     if ($erreur) return;
     $erreur = $text;
     if (!$send) return;

modules/register.php
@@ -196,6 +196,7 @@ class RegisterModule extends PLModule
                             $alert .= "Tentative d'inscription depuis une IP surveillee";
                         }
                         if ($email_banned || $ip_banned) {
+                            global $globals;
                             $err = "Une erreur s'est produite lors de l'inscription."
                                  . " Merci de contacter <a href='mailto:register@{$globals->mail->domain}>"
                                  . " register@{$globals->mail->domain}</a>"

modules/register/register.inc.php
@@ -148,6 +148,7 @@ function check_new_user(&$sub)
 
 function create_aliases (&$sub)
 {
+    global $globals;
     extract ($sub);
 
     require_once "xorg.misc.inc.php";