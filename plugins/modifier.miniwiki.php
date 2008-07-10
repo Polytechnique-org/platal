2ce986248cd0aad8fce405f188cf554da5610713
Florent Bruneau <florent.bruneau@polytechnique.org>
2008-07-10 22:49:27 +0200

    Oops
    Signed-off-by: Florent Bruneau <florent.bruneau@polytechnique.org>

--- plugins/modifier.miniwiki.php
+++ plugins/modifier.miniwiki.php
@@ -19,8 +19,6 @@
  *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
  ***************************************************************************/
 
-require_once 'platal.inc.php';
-
 function smarty_modifier_miniwiki($string, $format = 'no_title', $type = 'html')
 {
     if ($type == 'html') {
