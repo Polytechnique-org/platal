b8d9079780e3084e26949b018db319b61a801909
Stéphane Jacob <jacou@melix.net>
2008-07-23 08:00:13 +0200

    Uses PlPage instead of PlatalPage (again).

--- include/emails.combobox.inc.php
+++ include/emails.combobox.inc.php
@@ -19,7 +19,7 @@
  *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
  ***************************************************************************/
 
-function fill_email_combobox(PlatalPage& $page)
+function fill_email_combobox(PlPage& $page)
 {
     global $globals;
 
