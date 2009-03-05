5112531d976339fcb6be3846dd7f8754e7547311
Stéphane Jacob <jacou@melix.net>
2009-03-05 21:16:45 +0100

    Fixes addresses validation when the user chooses a geocoded address.

--- htdocs/javascript/profile.js
+++ htdocs/javascript/profile.js
@@ -306,6 +306,9 @@ function validGeoloc(id, geoloc)
         $('#addresses_' + id + '_cont').find('[name*=text]').val($('#addresses_' + id + '_cont').find('[name*=geoloc]').val());
         $('#addresses_' + id + '_cont').find('[name*=postalText]').val($('#addresses_' + id + '_cont').find('[name*=geocodedPostalText]').val());
     }
+    if (geoloc > 0) {
+        $('#addresses_' + id + '_cont').find("[name='addresses[" + id + "][geoloc]']").remove();
+    }
     $('#addresses_' + id + '_cont').find('[name*=text]').removeClass('error');
     $('#addresses_' + id + '_cont').find('[name*=geoloc_choice]').val(geoloc);
     $('.addresses_' + id + '_geoloc').remove();
