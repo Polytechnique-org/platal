81dcf5bf39babeb10144be17f5597f2a55e22223
Stéphane Jacob <sj@m4x.org>
2009-10-30 14:59:11 +0100

    Removes statistics about the profiles consultation: it was far too costly.

--- modules/stats.php
+++ modules/stats.php
@@ -41,7 +41,6 @@ class StatsModule extends PLModule
             'stats/graph'           => $this->make_hook('graph',     AUTH_COOKIE),
             'stats/graph/evolution' => $this->make_hook('graph_evo', AUTH_COOKIE),
             'stats/promos'          => $this->make_hook('promos',    AUTH_COOKIE),
-            'stats/profile'         => $this->make_hook('profile',   AUTH_COOKIE),
 
             'stats/coupures'        => $this->make_hook('coupures',  AUTH_PUBLIC),
         );
@@ -294,30 +293,6 @@ EOF2;
                                    WHERE  state != 'ok'");
             $page->assign('mxs', $res);
         }
-    }
-
-    function handler_profile(&$page, $period = 'overall')
-    {
-        $page->changeTpl('stats/profile.tpl');
-
-        $time = '';
-        switch ($period) {
-          case 'week': case 'month': case 'year':
-            $time = ' AND e.stamp > DATE_SUB(CURDATE(), INTERVAL 1 ' . strtoupper($period) . ')';
-            break;
-        }
-        $rows = XDB::iterator("SELECT  IF(u.nom_usage != '', u.nom_usage, u.nom) AS nom,
-                                       u.prenom, u.promo, e.data AS forlife, COUNT(*) AS count
-                                 FROM  logger.events AS e
-                           INNER JOIN  logger.actions AS act ON (e.action = act.id)
-                           INNER JOIN  aliases AS a ON (a.alias = e.data)
-                           INNER JOIN  auth_user_md5 AS u ON (u.user_id = a.id)
-                                WHERE  act.text = 'view_profile' $time
-                             GROUP BY  e.data
-                             ORDER BY  count DESC
-                                LIMIT  10");
-        $page->assign('profiles', $rows);
-        $page->assign('period', $period);
     }
 }
 
