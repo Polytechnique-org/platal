220cce87f8c3b96a80172ff9540c94ad257d4731
Florent Bruneau <florent.bruneau@polytechnique.org>
2010-04-11 11:44:55 +0200

    No more hacks to handle '%grp'.
    Signed-off-by: Florent Bruneau <florent.bruneau@polytechnique.org>

--- classes/xnet.php
+++ classes/xnet.php
@@ -25,52 +25,23 @@ class Xnet extends Platal
     {
         parent::__construct('xnet', 'xnetgrp', 'xnetlists', 'xnetevents',
                             'payment', 'bandeau');
+    }
 
-        global $globals;
-        if ($globals->asso()) {
-            if ($p = strpos($this->path, '/')) {
-                $this->ns   = substr($this->path, 0, $p).'/';
-                $this->path = '%grp'.substr($this->path, $p);
-            } else {
-                $this->ns   = $this->path.'/';
-                $this->path = '%grp';
+    public function hook_map($name)
+    {
+        if ($name == 'grp') {
+            global $globals;
+            if ($globals->asso()) {
+                return $globals->asso('shortname');
             }
         }
-    }
-
-    protected function find_nearest_key($key, array &$array)
-    {
-        global $globals;
-        if (in_array('%grp', array_keys($array)) &&  $key == $globals->asso('diminutif')) {
-            return '%grp';
-        }
-        return parent::find_nearest_key($key, $array);
-    }
-
-    public function near_hook()
-    {
-        global $globals;
-        $link = str_replace('%grp', $globals->asso('diminutif'), parent::near_hook());
-        if ($link != $this->path) {
-            return $link;
-        }
         return null;
-    }
-
-    public function pl_self($n = null)
-    {
-        global $globals;
-        return str_replace('%grp', $globals->asso('diminutif'), parent::pl_self($n));
     }
 
     protected function find_hook()
     {
         $ans = parent::find_hook();
         $this->https = false;
-        if ($ans && $this->ns) {
-            $this->path    = $this->ns . substr($this->path, 5);
-            $this->argv[0] = $this->ns . substr($this->argv[0], 5);
-        }
         return $ans;
     }
 
