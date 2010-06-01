bb9d47f4cee1ab76400d0ac147657b983f004114
Florent Bruneau <florent.bruneau@polytechnique.org>
2010-06-01 14:45:39 +0200

    Upgrade master is upgrade/1.0.0
    Signed-off-by: Florent Bruneau <florent.bruneau@polytechnique.org>

--- upgrade/1.0.0/update.sh
+++ upgrade/1.0.0/update.sh
@@ -2,14 +2,26 @@
 
 . ../inc/pervasive.sh
 
-mailman_stop
-mailman_templates
-mailman_start
+###########################################################
+[ "$DATABASE" != "x4dat" ] || die "Cannot target x4dat"
+
+confirm "Setting up new database: target db is \"$DATABASE\", source prefix is \"$DBPREFIX\""
+
+echo -n "* create database "
+(echo "CREATE DATABASE IF NOT EXISTS $DATABASE;" | mysql_pipe) || die "ERROR"
+echo "OK"
+
+echo -n "* copying tables "
+(../account/copy_tables.sh | mysql_pipe) || die "ERROR"
+echo "OK"
+
+mysql_run_directory ../newdirectory-0.0.1
+mysql_run_directory ../account
+mysql_run_directory .
 
 ###########################################################
-for sql in *.sql
-do
-    echo -n $sql
-    $MYSQL $DATABASE < $sql &>/dev/null || echo -n " ERROR"
-    echo .
-done
+confirm "Running upgrade scripts"
+script_run ../newdirectory-0.0.1/phones.php
+script_run ../newdirectory-0.0.1/addresses.php
+script_run ../newdirectory-0.0.1/alternate_subsubsectors.php
+script_run ../account/birthday.php
