453cbb0024d78fbbb3637874c0d4d92c3f89a9c1
Raphaël Barrois <raphael.barrois@polytechnique.org>
2010-07-01 18:00:46 +0200

    Minor typo
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

--- include/validations/names.inc.php
+++ include/validations/names.inc.php
@@ -19,7 +19,7 @@
  *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
  ***************************************************************************/
 
-// {{{ class NamesReq4
+// {{{ class NamesReq
 
 class NamesReq extends ProfileValidate
 {
