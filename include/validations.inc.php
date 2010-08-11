3c8c0f6467b4a9e333e3cd85d60d056162f741ed
Raphaël Barrois <raphael.barrois@polytechnique.org>
2010-08-11 11:45:23 +0200

    Revert "Might fix notification mail on name change.": Not a fix.
    This reverts commit 0aaf1f0671cb0ce8cafac6b1634290323ed4c9b4.

--- include/validations.inc.php
+++ include/validations.inc.php
@@ -529,9 +529,9 @@ abstract class ProfileValidate extends Validate
         $mailer = new PlMailer();
         $mailer->setSubject($this->_mail_subj());
         $mailer->setFrom("validation+{$this->type}@{$globals->mail->domain}");
-        $mailer->addTo("\"{$this->profile->fullName()}\" <{$this->profileOwner->forlifeEmail()}>");
+        $mailer->addTo("\"{$this->profile->fullName()}\" <{$this->profileOwner->bestEmail()}>");
         if (!$this->userIsProfileOwner) {
-            $mailer->addCc("\"{$this->user->fullName()}\" <{$this->user->forlifeEmail()}>");
+            $mailer->addCc("\"{$this->user->fullName()}\" <{$this->user->bestEmail()}>");
         }
         $mailer->addCc("validation+{$this->type}@{$globals->mail->domain}");
 
