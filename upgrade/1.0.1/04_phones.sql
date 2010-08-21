f168c01446837a3cf360c1fb7dd869bc8bb01dc9
Stéphane Jacob <sj@m4x.org>
2010-08-21 10:51:18 +0200

    Removes erroneous phones.
    Signed-off-by: Stéphane Jacob <sj@m4x.org>

--- upgrade/1.0.1/04_phones.sql
+++ upgrade/1.0.1/04_phones.sql
@@ -1,4 +1,6 @@
 ALTER TABLE profile_phones MODIFY COLUMN pid INT(6) NOT NULL DEFAULT 0;
 ALTER TABLE profile_phones MODIFY COLUMN link_id INT(6) NOT NULL DEFAULT 0;
 
+DELETE FROM profile_phones WHERE search_tel = '' OR display_tel = '' OR link_type = 'hq';
+
 -- vim:set syntax=mysql:
