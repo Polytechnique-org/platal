Polytechnique-org/platal

commit 7188601607762f52cf2688b4e97cedd5e36c8ad7

Fix advanced search when soundex is activated.
Signed-off-by: Florent Bruneau <florent.bruneau@polytechnique.org>

include/ufbuilder.inc.php
@@ -520,18 +520,8 @@ class UFBF_Quick extends UFB_Field
             } else {
                 $flags = array('public');
             }
-            if ($ufb->b('with_soundex')) {
-                $soundex = true;
-                $st = array();
-                foreach ($strings as $string) {
-                    $st[] = soundex_fr($string);
-                }
-            } else {
-                $soundex = false;
-                $st = $strings;
-            }
             $exact =$ufb->b('exact');
-            $conds->addChild(new UFC_NameTokens($st, $flags, $soundex, $exact));
+            $conds->addChild(new UFC_NameTokens($st, $flags, $ufb->b('with_soundex'), $exact));
 
             $ufb->addOrder(new UFO_Score());
         }

classes/userfilter.php
@@ -414,7 +414,7 @@ class UFC_NameTokens implements UserFilterCondition
         foreach ($this->tokens as $i => $token) {
             $sub = $uf->addNameTokensFilter($token);
             if ($this->soundex) {
-                $c = XDB::format($sub . '.soundex = {?}', $token);
+                $c = XDB::format($sub . '.soundex = {?}', soundex_fr($token));
             } else if ($this->exact) {
                 $c = XDB::format($sub . '.token = {?}', $token);
             } else {

ut/userfiltertest.php
@@ -424,16 +424,13 @@ class UserFilterTest extends PlTestCase
             // soundex, !exact
             array(self::buildProfileQuery('LEFT JOIN  search_name AS sn ON (p.pid = sn.pid)
                                                WHERE  sn.soundex = \'XLNO\''),
-                new UFC_NameTokens('XLNO', array(), true), -1),
+                new UFC_NameTokens('xelnor', array(), true), -1),
             array(self::buildProfileQuery('LEFT JOIN  search_name AS sn ON (p.pid = sn.pid)
                                                WHERE  sn.soundex IN (\'XLNO\', \'BROS\')'),
-                new UFC_NameTokens(array('XLNO', 'BROS'), array(), true), -1),
-            array(self::buildProfileQuery('LEFT JOIN  search_name AS sn ON (p.pid = sn.pid)
-                                               WHERE  sn.soundex = \'ZZZZZZ\''),
-                new UFC_NameTokens('ZZZZZZ', array(), true), 0),
+                new UFC_NameTokens(array('xelnor', 'barrois'), array(), true), -1),
             array(self::buildProfileQuery('LEFT JOIN  search_name AS sn ON (p.pid = sn.pid)
                                                WHERE  sn.soundex = \'BROS\' AND FIND_IN_SET(\'public\', sn.flags)'),
-                new UFC_NameTokens('BROS', UFC_NameTokens::FLAG_PUBLIC, true), -1),
+                new UFC_NameTokens('barrois', UFC_NameTokens::FLAG_PUBLIC, true), -1),
         );
 
         /* UFC_Nationality

commit 9d590571b644fb7851c0c98f57051da86549b20b

Fix quick search broken by 7188601607762f52cf2688b4e97cedd5e36c8ad7.
Signed-off-by: Florent Bruneau <florent.bruneau@polytechnique.org>

include/ufbuilder.inc.php
@@ -521,7 +521,7 @@ class UFBF_Quick extends UFB_Field
                 $flags = array('public');
             }
             $exact =$ufb->b('exact');
-            $conds->addChild(new UFC_NameTokens($st, $flags, $ufb->b('with_soundex'), $exact));
+            $conds->addChild(new UFC_NameTokens($strings, $flags, $ufb->b('with_soundex'), $exact));
 
             $ufb->addOrder(new UFO_Score());
         }