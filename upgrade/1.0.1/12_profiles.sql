60db728a2709b1ee2d53fd3162644eb8dc184a7d
Stéphane Jacob <sj@m4x.org>
2010-10-21 09:50:52 +0200

    Uncomment foreign keys.
    Signed-off-by: Stéphane Jacob <sj@m4x.org>

--- upgrade/1.0.1/12_profiles.sql
+++ upgrade/1.0.1/12_profiles.sql
@@ -8,8 +8,8 @@ CREATE TABLE profile_modifications (
   newText TEXT NOT NULL,
   pub ENUM('private', 'ax', 'public') NOT NULL DEFAULT 'private',
   PRIMARY KEY(pid, uid, field)
-  #FOREIGN KEY (uid) REFERENCES accounts (uid) ON DELETE CASCADE ON UPDATE CASCADE,
-  #FOREIGN KEY (pid) REFERENCES profiles (pid) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (uid) REFERENCES accounts (uid) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (pid) REFERENCES profiles (pid) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB, CHARSET=utf8;
 
 -- vim:set syntax=mysql:
