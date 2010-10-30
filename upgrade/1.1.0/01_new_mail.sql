b0e71a9f5b929cdb27535a4a1ba56d9e7a307856
João Pedro Athayde Marcondes de André (JP) <jp_git@m4x.org>
2010-10-30 21:51:00 +0200

    Add drop tables

--- upgrade/1.1.0/01_new_mail.sql
+++ upgrade/1.1.0/01_new_mail.sql
@@ -450,4 +450,14 @@ DELETE FROM email_virtual WHERE alias LIKE "tech-email%@polytechnique.org";
 DELETE FROM email_virtual WHERE alias LIKE "tech-email%@m4x.org";
 DELETE FROM email_virtual WHERE alias LIKE "tech-email%@m4x.net";
 
+/*********************************************************************************************************************************/
+DROP TABLE aliases;
+DROP TABLE virtual;
+DROP TABLE virtual_redirect;
+DROP TABLE homonyms;
+DROP TABLE emails;
+DROP TABLE email_options;
+DROP TABLE domain;
+
+
 -- vim:set syntax=mysql:
