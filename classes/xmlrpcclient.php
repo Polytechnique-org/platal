502bb918559d93793538b8d9bc451600fbb9c027
Florent Bruneau <florent.bruneau@polytechnique.org>
2010-12-15 13:43:41 +0100

    In case of RPC error, show the HTTP answer.
    Signed-off-by: Florent Bruneau <florent.bruneau@polytechnique.org>

--- classes/xmlrpcclient.php
+++ classes/xmlrpcclient.php
@@ -123,7 +123,7 @@ class XmlrpcClient
             }
         } else {
             Platal::assert(!is_array($result) && isset($result['faultCode']),
-                           "RPC Error:\n" . $result['faultString'],
+                           "RPC Error:\n" . $answer,
                            "Une erreur est survenue lors de l'accès aux listes de diffusion.");
         }
 
