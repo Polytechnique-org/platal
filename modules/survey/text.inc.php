5e1513f67936a6c6866113d260746711af4ea2ee
Florent Bruneau <florent.bruneau@polytechnique.org>
2011-01-04 21:51:37 +0100

    Happy New Year!
    Signed-off-by: Florent Bruneau <florent.bruneau@polytechnique.org>

--- modules/survey/text.inc.php
+++ modules/survey/text.inc.php
@@ -1,6 +1,6 @@
 <?php
 /***************************************************************************
- *  Copyright (C) 2003-2010 Polytechnique.org                              *
+ *  Copyright (C) 2003-2011 Polytechnique.org                              *
  *  http://opensource.polytechnique.org/                                   *
  *                                                                         *
  *  This program is free software; you can redistribute it and/or modify   *
