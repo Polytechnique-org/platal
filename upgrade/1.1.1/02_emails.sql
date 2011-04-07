42c3833ff847814f7177863f37eed8221080606b
Stéphane Jacob <sj@m4x.org>
2011-04-07 14:49:43 +0200

    Updates email_virtual so that it only contains forlife emails.
    Signed-off-by: Stéphane Jacob <sj@m4x.org>

---
+++ upgrade/1.1.1/02_emails.sql
@@ -0,0 +1,9 @@
+    DELETE  e2
+      FROM  email_virtual AS e1
+INNER JOIN  email_virtual AS e2
+     WHERE  e1.email = e2.email AND e1.domain = e2.domain AND e1.redirect = REPLACE(e2.redirect, '@m4x.org', '@polytechnique.org');
+UPDATE  email_virtual
+   SET  redirect = REPLACE(redirect, '@m4x.org', '@polytechnique.org')
+ WHERE  redirect LIKE '%@m4x.org';
+
+-- vim:set syntax=mysql:
