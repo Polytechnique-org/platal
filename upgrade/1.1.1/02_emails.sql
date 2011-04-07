120baaa58370780550b601cac34df2cd9f637fe0
Stéphane Jacob <sj@m4x.org>
2011-04-07 21:31:34 +0200

    Oops.
    Signed-off-by: Stéphane Jacob <sj@m4x.org>

--- upgrade/1.1.1/02_emails.sql
+++ upgrade/1.1.1/02_emails.sql
@@ -1,7 +1,8 @@
     DELETE  e2
       FROM  email_virtual AS e1
 INNER JOIN  email_virtual AS e2
-     WHERE  e1.email = e2.email AND e1.domain = e2.domain AND e1.redirect = REPLACE(e2.redirect, '@m4x.org', '@polytechnique.org');
+     WHERE  e1.email = e2.email AND e1.domain = e2.domain AND e1.redirect != e2.redirect
+            AND e1.redirect = REPLACE(e2.redirect, '@m4x.org', '@polytechnique.org');
 UPDATE  email_virtual
    SET  redirect = REPLACE(redirect, '@m4x.org', '@polytechnique.org')
  WHERE  redirect LIKE '%@m4x.org';
