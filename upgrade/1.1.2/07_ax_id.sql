f856ff2ed13afec516816057913a7490a19ee5e7
Stéphane Jacob <sj@m4x.org>
2011-06-17 23:26:41 +0200

    We have 3 new ax ids :)
    Signed-off-by: Stéphane Jacob <sj@m4x.org>

---
+++ upgrade/1.1.2/07_ax_id.sql
@@ -0,0 +1,5 @@
+UPDATE profiles SET ax_id = '2008M151' WHERE pid = 44992;
+UPDATE profiles SET ax_id = '2004M043' WHERE pid = 46435;
+UPDATE profiles SET ax_id = '2004M038' WHERE pid = 45914;
+
+-- vim:set syntax=mysql:
