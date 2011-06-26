ee67c55ff5dd967cbedb1e0065b2d52b626f74f7
Stéphane Jacob <sj@m4x.org>
2011-06-26 14:00:23 +0200

    Adds new ax_id.
    Signed-off-by: Stéphane Jacob <sj@m4x.org>

--- upgrade/1.1.2/07_ax_id.sql
+++ upgrade/1.1.2/07_ax_id.sql
@@ -1,5 +1,6 @@
 UPDATE profiles SET ax_id = '2008M151' WHERE pid = 44992;
 UPDATE profiles SET ax_id = '2004M043' WHERE pid = 46435;
 UPDATE profiles SET ax_id = '2004M038' WHERE pid = 45914;
+UPDATE profiles SET ax_id = '2008M169' WHERE pid = 45010;
 
 -- vim:set syntax=mysql:
