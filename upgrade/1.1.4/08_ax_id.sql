44d239f67b4724c92abdec680c8cd31ac9e36649
Stéphane Jacob <sj@m4x.org>
2011-10-11 14:45:53 +0200

    Synchronises another few students with ax data.
    Signed-off-by: Stéphane Jacob <sj@m4x.org>

---
+++ upgrade/1.1.4/08_ax_id.sql
@@ -0,0 +1,6 @@
+UPDATE profiles SET ax_id = '2008D046' WHERE pid = 45818;
+UPDATE profiles SET ax_id = '2009D059' WHERE pid = 45855;
+UPDATE profiles SET ax_id = '2009D369' WHERE pid = 46232;
+UPDATE profiles SET ax_id = '2009D221' WHERE pid = 46016;
+
+-- vim:set syntax=mysql:
