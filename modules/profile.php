2e04c41b702b2ff21bc0bd9cee5fe119eb863950
Stéphane Jacob <sj@m4x.org>
2011-10-25 00:28:38 +0200

    I should have merged before last commit...
    Signed-off-by: Stéphane Jacob <sj@m4x.org>

--- modules/profile.php
+++ modules/profile.php
@@ -273,7 +273,7 @@ class ProfileModule extends PLModule
         $page->setTitle($profile->fullName());
 
         // Determines and displays the virtual alias.
-        if (!is_null($owner) && $profile->alias_pub == 'public') {
+        if (!is_null($owner) && $profile->isVisible($profile->alias_pub)) {
             $page->assign('virtualalias', $owner->emailAlias());
         }
 
