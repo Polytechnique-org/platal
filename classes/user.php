Polytechnique-org/platal

commit 7de5cd885589022b78b29d7881ec9922408339d6

Idendifies user with partial match against hruid.
Signed-off-by: Stéphane Jacob <sj@m4x.org>

classes/user.php
@@ -83,9 +83,9 @@ class User extends PlUser
         }
 
         // Checks whether $login is a valid hruid or not.
-        $res = XDB::query('SELECT  uid
+        $res = XDB::query("SELECT  uid
                              FROM  accounts
-                            WHERE  hruid = {?}', $login);
+                            WHERE  hruid LIKE CONCAT('%', {?}, '%')", $login);
         if ($res->numRows()) {
             return $res->fetchOneCell();
         }

commit bfc34dc5b61be15b1d3eea7adf651bf9390bc6ad

Fixes security issue in User::getLogin introduced in 7de5cd885589022b78b29d7881ec9922408339d6.
Signed-off-by: Stéphane Jacob <sj@m4x.org>

classes/user.php
@@ -83,10 +83,10 @@ class User extends PlUser
         }
 
         // Checks whether $login is a valid hruid or not.
-        $res = XDB::query("SELECT  uid
+        $res = XDB::query('SELECT  uid
                              FROM  accounts
-                            WHERE  hruid LIKE CONCAT('%', {?}, '%')", $login);
-        if ($res->numRows()) {
+                            WHERE  hruid' . XDB::formatWildcards(XDB::WILDCARD_CONTAINS, $login));
+        if ($res->numRows() == 1) {
             return $res->fetchOneCell();
         }
 