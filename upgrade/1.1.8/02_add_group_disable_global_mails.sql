59d8146a22f94cef1cfab62de6e9b41f80773137
Raphaël Barrois <raphael.barrois@polytechnique.org>
2013-04-25 00:30:47 +0200

    xnetgrp: Disable 'send emails' for new groups.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

--- upgrade/1.1.8/02_add_group_disable_global_mails.sql
+++ upgrade/1.1.8/02_add_group_disable_global_mails.sql
@@ -1,3 +1,4 @@
-ALTER TABLE groups ADD COLUMN disable_mails int(1) NOT NULL DEFAULT 0;
+ALTER TABLE groups ADD COLUMN disable_mails int(1) NOT NULL DEFAULT 1;
+UPDATE groups SET disable_mails = 0;
 
 -- vim:set syntax=mysql:
