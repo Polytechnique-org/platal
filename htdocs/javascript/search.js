Polytechnique-org/platal

commit f9dfb8bd900bad297db24944b69bb8170c2dcb2d

Allows search on both only_referent and compagny fields (Closes #1513).
Signed-off-by: Stéphane Jacob <sj@m4x.org>

htdocs/javascript/search.js
@@ -323,16 +323,6 @@ function changeSchool(schoolId, diploma)
     });
 }
 
-// when checking/unchecking "only_referent", disable/enable some fields
-function changeOnlyReferent()
-{
-    if ($("#only_referent").is(':checked')) {
-        $("input[name='entreprise']").attr('disabled', true);
-    } else {
-        $("input[name='entreprise']").removeAttr('disabled');
-    }
-}
-
 // when choosing a job term in tree, hide tree and set job term field
 function searchForJobTerm(treeid, jtid, full_name)
 {

include/ufbuilder.inc.php
@@ -1132,34 +1132,9 @@ class UFBF_AddressIndex extends UFBF_Index
 // {{{ class UFBF_JobCompany
 class UFBF_JobCompany extends UFBF_Text
 {
-    private $onlymentorfield;
-
-    public function __construct($envfield, $formtext = '', $onlymentorfield = 'only_referent')
-    {
-        parent::__construct($envfield, $formtext);
-        $this->onlymentorfield = $onlymentorfield;
-    }
-
-    public function check(UserFilterBuilder $ufb) {
-        if (parent::check($ufb)) {
-            # No company check for mentors
-           if ($ufb->isOn($this->onlymentorfield)) {
-                $this->empty = true;
-           }
-           return true;
-        } else {
-            return false;
-        }
-    }
-
     protected function buildUFC(UserFilterBuilder $ufb)
     {
         return new UFC_Job_Company(UFC_Job_Company::JOBNAME, $this->val);
-    }
-
-    public function getEnvFieldNames()
-    {
-        return array($this->envfield, $this->onlymentorfield);
     }
 }
 // }}}

commit e647f84324613cd4db5d8304fc8e275ee536dc4b

Remove Javascript call to undefined function changeOnlyReferent()
Bug introduced by commit f9dfb8bd "Allows search on both only_referent and compagny fields"
http://git.polytechnique.org/?p=platal.git;a=commitdiff;h=f9dfb8bd900bad297db24944b69bb8170c2dcb2d

Signed-off-by: Nicolas Iooss <nicolas.iooss_git@polytechnique.org>

htdocs/javascript/search.js
@@ -86,8 +86,6 @@ function load_advanced_search(request)
         });
     });
 
-    $('#only_referent').change(function() { changeOnlyReferent(); });
-
     $('.delete_address_component').click(function() {
         var field_name = $(this).attr('href');
         var hide = false;