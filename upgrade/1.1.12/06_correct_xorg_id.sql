33b8aa2fab6873f1bfa8c054cf1204c31f30ee79
Anne Limoges <anne.limoges_git@polytechnique.org>
2014-03-04 21:59:24 +0100

    Corrects xorg_id.

--- upgrade/1.1.12/06_correct_xorg_id.sql
+++ upgrade/1.1.12/06_correct_xorg_id.sql
@@ -1,1 +1,6 @@
+UPDATE profiles SET xorg_id = 19820179 WHERE pid = 48114;
 UPDATE profiles SET xorg_id = 19860296 WHERE pid = 48884;
+UPDATE profiles SET xorg_id = 220050001 WHERE pid = 48886;
+UPDATE profiles SET xorg_id = 220060032 WHERE pid = 48887;
+UPDATE profiles SET xorg_id = 220060078 WHERE pid = 48889;
+UPDATE profiles SET xorg_id = 219970030 WHERE pid = 48890;
