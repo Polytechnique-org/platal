b871ef3db91aa9f920022d964407727150a26dc3
Thomas Minvielle <thomas.minvielle@polytechnique.org>
2014-03-15 11:24:48 +0100

    Improve database definition.

--- upgrade/manageurs/01_create_profile_manageurs.sql
+++ upgrade/manageurs/01_create_profile_manageurs.sql
@@ -6,7 +6,7 @@ CREATE TABLE profile_manageurs (
     anonymity TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
     visibility ENUM('visible', 'visible_exceptions', 'blocked') NOT NULL DEFAULT 'blocked',
     email VARCHAR(255) NOT NULL DEFAULT '',
-    communication SET('novelties', 'nl', 'survey') NOT NULL DEFAULT '',
+    communication SET('novelty', 'nl', 'survey') NOT NULL DEFAULT '',
     push ENUM('unique', 'weekly', 'never') NOT NULL DEFAULT 'never',
     network TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
     PRIMARY KEY (pid),
