0350128986bad6fc5dec530a1d1ddcf1e7d5a5e1
Thomas Minvielle <thomas.minvielle@polytechnique.org>
2014-05-17 14:40:01 +0200

    Master deletion.

--- upgrade/1.1.13/02_deleteMaster.sql
+++ upgrade/1.1.13/02_deleteMaster.sql
@@ -1,2 +1,4 @@
 DELETE FROM profile_education WHERE pid = 41787 AND id = 101;
-
+DELETE FROM profile_education WHERE pid = 41976 AND id = 101;
+DELETE FROM profile_education WHERE pid = 42172 AND id = 101;
+DELETE FROM profile_education WHERE pid = 43002 AND id = 101;
