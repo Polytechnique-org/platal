c46fa5e5fa86a14286ad21d3df3476d0ae3511fd
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2014-12-07 17:32:41 +0100

    Fix MySQL upgrade script
    In MySQL, there is no star between DELETE and FROM

--- upgrade/1.1.14/02_delete_unlinked_rows.sql
+++ upgrade/1.1.14/02_delete_unlinked_rows.sql
@@ -1,7 +1,7 @@
-DELETE * FROM profile_addresses WHERE pid = 44872 AND jobid = 0 AND groupid = 0 AND type = 'home' AND id = 0;
-DELETE * FROM profile_addresses WHERE pid = 16669 AND jobid = 0 AND groupid = 0 AND type = 'job' AND id = 0;
-DELETE * FROM profile_addresses WHERE pid = 44872 AND jobid = 0 AND groupid = 0 AND type = 'job' AND id = 0;
-DELETE * FROM profile_phones WHERE pid = 16669 AND link_type = 'pro' AND link_id = 0 AND tel_id = 0;
-DELETE * FROM profile_phones WHERE pid = 44872 AND link_type = 'pro' AND link_id = 0 AND tel_id = 0;
-DELETE * FROM profile_phones WHERE pid = 44872 AND link_type = 'user' AND link_id = 0 AND tel_id = 0;
-DELETE * FROM profile_phones WHERE pid = 44872 AND link_type = 'user' AND link_id = 0 AND tel_id = 1;
+DELETE FROM profile_addresses WHERE pid = 44872 AND jobid = 0 AND groupid = 0 AND type = 'home' AND id = 0;
+DELETE FROM profile_addresses WHERE pid = 16669 AND jobid = 0 AND groupid = 0 AND type = 'job' AND id = 0;
+DELETE FROM profile_addresses WHERE pid = 44872 AND jobid = 0 AND groupid = 0 AND type = 'job' AND id = 0;
+DELETE FROM profile_phones WHERE pid = 16669 AND link_type = 'pro' AND link_id = 0 AND tel_id = 0;
+DELETE FROM profile_phones WHERE pid = 44872 AND link_type = 'pro' AND link_id = 0 AND tel_id = 0;
+DELETE FROM profile_phones WHERE pid = 44872 AND link_type = 'user' AND link_id = 0 AND tel_id = 0;
+DELETE FROM profile_phones WHERE pid = 44872 AND link_type = 'user' AND link_id = 0 AND tel_id = 1;
