Polytechnique-org/platal

commit dc274d445809c5fae4c5c49bf0bd2b7fba195587

Fix the birthday.send code.
Use the cleaner MailingList interface, in sudo mode.

bin/lists.rpc.py
@@ -483,6 +483,17 @@ def get_members(userdesc, perms, mlist):
     return (details, members, mlist.owner)
 
 
+def get_subscription_status(userdesc, perms, mlist):
+    """ Retrieve the subscription status of a user.
+            @mlist
+    """
+    infos = get_list_info(userdesc, perms, mlist)
+    if infos is None:
+        return -1  # ENOACCESS
+    details, _members = infos
+    return details['sub']
+
+
 #-------------------------------------------------------------------------------
 # users procedures for [ trombi.php ]
 #

bin/cron/birthday.send.php
@@ -65,15 +65,10 @@ while($profile = $iterator->next()) {
     // Check if the user subscribed to the promo ML
     $mlpromo = false;
     if ($isX && $promoX) {
-        $listClient = new MMList($user, $promoX . '.' . $globals->mail->domain);
-        $mlists = $listClient->get_lists($user->forlifeEmail());
-        foreach ($mlists as $mlist) {
-            if ($mlist['list'] == 'promo') {
-                // $mlist['sub'] is 0 for not-subscribed, 1 for pending and 2 for subscribed
-                $mlpromo = ($mlist['sub'] >= 1);
-                break;
-            }
-        }
+        // Load the MailingList in 'sudo' mode: the user might not be fully active
+        $promo_list = MailingList::promo($promoX, $user, true);
+        $mlpromo_sub = $promo_list->subscriptionState();
+        $mlpromo = ($mlpromo_sub == MailingList::SUB_PENDING || $mlpromo_sub == MailingList::SUB_SUBSCRIBED);
     }
     $mailer->assign('mlpromo', $mlpromo);
     $mailer->assign('nlAX', NewsLetter::forGroup(NewsLetter::GROUP_AX)->subscriptionState($user));

include/mailinglist.inc.php
@@ -161,6 +161,18 @@ class MailingList
         return $this->mmclient->get_members_limit($this->mbox, $page, $number_per_page);
     }
 
+    const SUB_ENOACCESS = -1;
+    const SUB_NOTSUBSCRIBED = 0;
+    const SUB_PENDING = 1;
+    const SUB_SUBSCRIBED = 2;
+
+    /** Check the subscription status of the user
+     */
+    public function subscriptionState()
+    {
+        return $this->mmclient->get_subscription_status($this->mbox);
+    }
+
     /** Fetch pending list operations.
      *
      * TODO: document the return type

commit 92399c100f22ff2b04f4999bce34cf6fcad86c90

Register get_subscription_status function in lists RPC
This makes $promo_list->subscriptionState() actually works.

Fixes: dc274d445809 ("Fix the birthday.send code.")

bin/lists.rpc.py
@@ -1129,6 +1129,7 @@ server.register_function(subscribe)
 server.register_function(unsubscribe)
 # members.php
 server.register_function(get_members)
+server.register_function(get_subscription_status)
 # trombi.php
 server.register_function(get_members_limit)
 server.register_function(get_owners)