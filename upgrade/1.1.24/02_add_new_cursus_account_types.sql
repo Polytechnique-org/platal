652e8ed608fb73061fbc0bf50dba23ad79c7dca0
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2018-04-22 16:55:14 +0200

    Add new cursus to account_types table

---
+++ upgrade/1.1.24/02_add_new_cursus_account_types.sql
@@ -0,0 +1,6 @@
+INSERT INTO `account_types` SET type='bachelor', perms='groups,mail,directory_private,forums,lists,payment,user', description="Bachelor de l'X";
+INSERT INTO `account_types` SET type='executive', perms='groups,mail,directory_private,forums,lists,payment,user', description="Executive Education de l'X";
+INSERT INTO `account_types` SET type='graduate', perms='groups,mail,directory_private,forums,lists,payment,user', description="Graduate Degree de l'X";
+INSERT INTO `account_types` SET type='masterspe', perms='groups,mail,directory_private,forums,lists,payment,user', description="Master spécialisé de l'X";
+
+-- vim:set syntax=mysql:
