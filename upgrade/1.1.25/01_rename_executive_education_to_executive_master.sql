db3b1d6471f51081e80ca351dc106819bd7076a5
PositroniumJS <150566116+PositroniumJS@users.noreply.github.com>
2026-04-19 17:02:51 +0200

    Add SQL script to rename Executive Education to Executive Master in the database

--- upgrade/1.1.25/01_rename_executive_education_to_executive_master.sql
+++ upgrade/1.1.25/01_rename_executive_education_to_executive_master.sql
@@ -1,2 +1,5 @@
-# TODO fill it up
-# Tables to edit: `account_types`, `profile_education_degree_enum`
+-- Rename Executive Education to Executive Master
+UPDATE `profile_education_degree_enum` SET degree='Executive Master', abbreviation='EM' WHERE degree='Executive Education';
+UPDATE `account_types` SET description="Executive Master de l'X" WHERE type='executive';
+
+-- vim:set syntax=mysql:
